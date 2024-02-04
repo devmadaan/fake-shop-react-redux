033bbbb57a43ebd5a7f2a6ceef191f11bf5cd1b3
Dev Madaan <58107341+devmadaan@users.noreply.github.com>
2024-02-04 12:57:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/header";
+import "./App.css";
+import ProductDetails from "./containers/ProductDetails";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<ProductListing />} />
+          <Route
+            path="/product/:productId"
+            exact
+            element={<ProductDetails />}
+          />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+      {/* <div> Hello</div> */}
+    </div>
+  );
+}
+
+export default App;
